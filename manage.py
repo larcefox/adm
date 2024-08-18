4deab3e62936d8c74a6a25e3e9414fefda226757
Larce Fox <larcethefox@gmail.com>
2024-08-18 22:02:26 +0300

    run websocket in thred

--- manage.py
+++ manage.py
@@ -1,17 +1,20 @@
 from flask.cli import FlaskGroup
-import subprocess
+from threading import Thread
 from loguru import logger
 from src import app
+from lib.websocket_server import run_websocket
 
 
 logger.add('./logs/manage.log', format="{time} {level} {message}", level="INFO", retention="10 days")
 cli = FlaskGroup(app)
 
 if __name__ == "__main__":
-    command = ["/home/larce/projects/idm/.venv/bin/python", "/home/larce/projects/idm/lib/websocket_server.py"]
+    
+    ws_thread = Thread(target = run_websocket)
     try:
-        process = subprocess.Popen(command)
-        logger.info("Websocket trying to start:", process)
+        ws_thread.start()
+        # ws_thread.join()
+        logger.info("Websocket trying to start:", ws_thread)
         
     except OSError as e:
         logger.info("Websocket already started")
