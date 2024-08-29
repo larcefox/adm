19676d05543b4f5cf15f7570de03961e48e56600
larce <larcethefox@gmail.com>
2024-08-29 19:05:40 +0300

    radio works with nginx config

--- lib/radio.py
+++ lib/radio.py
@@ -10,7 +10,7 @@ radio_8bit = "http://myradio24.org/85516"
 radio_ambient = "http://ice6.somafm.com/deepspaceone-128-aac"
 radio_metal = "http://uk1.internet-radio.com:8294/live"
 
-@app.route('/8bit')
+@app.route('/radio/8bit')
 def stream_radio_8bit():
     def generate():
         with requests.get(radio_8bit, stream=True) as r:
@@ -20,7 +20,7 @@ def stream_radio_8bit():
     
     return Response(generate(), content_type="audio/mpeg")
 
-@app.route('/ambient')
+@app.route('/radio/ambient')
 def stream_radio_ambient():
     def generate():
         with requests.get(radio_ambient, stream=True) as r:
@@ -30,7 +30,7 @@ def stream_radio_ambient():
     
     return Response(generate(), content_type="audio/mpeg")
 
-@app.route('/metal')
+@app.route('/radio/metal')
 def stream_radio_metal():
     def generate():
         with requests.get(radio_metal, stream=True) as r:
@@ -40,4 +40,4 @@ def stream_radio_metal():
     
     return Response(generate(), content_type="audio/mpeg")
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8100)
+    app.run(host='127.0.0.1', port=8100)
