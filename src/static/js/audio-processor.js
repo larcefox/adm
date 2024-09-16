ed1e03f879a86bcdbec501f32a3b6e509201ad93
Larce Fox <larcethefox@gmail.com>
2024-09-16 23:36:26 +0300

    Fix other user model visualisation, sound quality better

--- src/static/js/audio-processor.js
+++ src/static/js/audio-processor.js
@@ -1,20 +1,28 @@
 class AudioProcessor extends AudioWorkletProcessor {
-    constructor() {
-        super();
+  constructor() {
+    super();
+    this.buffer = []; // Array to accumulate audio data
+    this.chunkSize = 4096; // Adjust chunk size to your desired value (e.g., 4096 samples)
+  }
+
+  process(inputs, outputs, parameters) {
+    // inputs[0][0] contains the audio data (first input channel)
+    if (inputs[0] && inputs[0][0]) {
+      const inputData = inputs[0][0];
+
+      // Accumulate audio data in the buffer
+      this.buffer.push(...inputData);
+
+      // If the buffer reaches the chunk size, send the data to the main thread
+      if (this.buffer.length >= this.chunkSize) {
+        const chunk = this.buffer.slice(0, this.chunkSize);
+        this.port.postMessage(chunk); // Send the chunk to the main thread
+        this.buffer = this.buffer.slice(this.chunkSize); // Remove the sent chunk from the buffer
+      }
     }
 
-    // This method processes the audio data
-    process(inputs, outputs, parameters) {
-        const input = inputs[0];
-        if (input.length > 0) {
-            // Get the first channel's data
-            const channelData = input[0];
-
-            // Send the audio data to the main thread
-            this.port.postMessage(channelData);
-        }
-        return true; // Keep the processor alive
-    }
+    return true; // Keep the processor running
+  }
 }
 
 registerProcessor('audio-processor', AudioProcessor);
