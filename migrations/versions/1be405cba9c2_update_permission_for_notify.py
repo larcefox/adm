df8171f040e3edd07ab1ed2d788be758c913fba8
larce <larcethefox@gmail.com>
2025-06-18 21:00:26 +0300

    update permissions in DB

---
+++ migrations/versions/1be405cba9c2_update_permission_for_notify.py
@@ -0,0 +1,26 @@
+"""update permission for notify
+
+Revision ID: 1be405cba9c2
+Revises: d88e8d6792e7
+Create Date: 2025-06-18 20:56:16.556234
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+from dotenv import load_dotenv
+import os
+
+# revision identifiers, used by Alembic.
+revision = '1be405cba9c2'
+down_revision = 'd88e8d6792e7'
+branch_labels = None
+depends_on = None
+load_dotenv()
+database_user = os.getenv('DATABASE_USER')
+
+def upgrade():
+    op.execute(f"GRANT EXECUTE ON FUNCTION pg_notify(text, text) TO {database_user};")
+
+def downgrade():
+    op.execute(f"REVOKE EXECUTE ON FUNCTION pg_notify(text, text) FROM {database_user};")
